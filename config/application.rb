e0d937892b8f2c7527188455ae6ce0f2e89deb2f
andela-gogbara <godwin.onisofien@andela.com>
2016-11-17 06:50:39 +0100

    default-layout-and-homepage

--- config/application.rb
+++ config/application.rb
@@ -6,5 +6,6 @@ Bundler.require(*Rails.groups)
 module LumumbaProd
   class Application < Rails::Application
     config.autoload_paths += Dir[Rails.root.join("app/lib")]
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
