9723deccdc788dc74405890a249e7a38c5583df7
vemv <vemv@users.noreply.github.com>
2016-11-19 21:11:12 +0100

    Remove turbolinks, as it's unused

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require cloudinary
 // require cloudinary/processing // Optional - client side processing (resizing and validation)
 //= require_tree .
