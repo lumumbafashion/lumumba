c417eae0333de6e87e1a55f5c9a453b93a843ad3
vemv <vemv@users.noreply.github.com>
2016-11-19 18:58:25 +0100

    Enable smtp for staging

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-if %w(staging production).include?(ENV['RAILS_ENV'])
+if %w(staging production).include?(ENV['RAILS_ENV'] || ENV['RACK_ENV'])
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
