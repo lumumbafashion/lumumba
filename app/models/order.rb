bd8948845fb25b62055b75e233d1000e9596f916
vemv <vemv@users.noreply.github.com>
2016-11-21 22:34:05 +0100

    Replace Braintree with Stripe

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,8 @@
 class Order < ApplicationRecord
 
   OPEN = 'open'
+  SUCCESSFULLY_PAID = 'successfully_paid'
+  STRIPE_EUR = 'eur'
 
   belongs_to :user
   has_many :order_items
@@ -8,8 +10,11 @@ class Order < ApplicationRecord
   validates :user, presence: true
   validates :order_number, presence: true
   validates :total_amount, presence: true, unless: :open?
-  validates :payment_method, presence: true, unless: :open?
   validates :status, presence: true
+  validates :transaction_id, presence: true, unless: :open?
+  if false
+    validates :payment_method, presence: true, unless: :open?
+  end
 
   after_initialize :set_default_values
 
@@ -25,12 +30,60 @@ class Order < ApplicationRecord
     where(status: OPEN)
   end
 
+  def self.charge_description_for order
+    "Lumumba - order id ##{order.id}"
+  end
+
+  def description
+    self.class.charge_description_for self
+  end
+
   def open?
     self.status == OPEN
   end
 
   def total_amount_formatted
     total_amount.round(2).to_s
+  end
+
+  def total_amount_in_cents
+    (total_amount * 100.0).round
+  end
+
+  def charged?
+    transaction_id.present?
+  end
+
+  def charge!(token)
+
+    raise "already charged" if charged?
+
+    charge = nil
+
+    charge = Stripe::Charge.create(
+      amount: total_amount_in_cents,
+      currency: STRIPE_EUR,
+      source: token,
+      description: self.class.charge_description_for(self)
+    )
+
+    Rails.logger.info "Successfully charged order with id #{id}. Charge id: #{charge.id}"
+
+    self.transaction_id = charge.id
+    self.status = SUCCESSFULLY_PAID
+    saved = self.save
+    unless saved
+      message = "Stripe charge succeeded, but could not update the order with id #{self.id}. Errors: #{self.errors.full_messages}"
+      Rails.logger.error message
+      Rollbar.error message
+    end
+    return true
+
+  rescue Stripe::CardError => e
+    Rails.logger.error e.class
+    Rails.logger.error e
+    Rollbar.error e
+    return false
   end
 
 end
