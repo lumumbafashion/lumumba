fdc7d584f7d0305f7fd3d78e31ad9bd2469a0cc8
vemv <vemv@users.noreply.github.com>
2016-11-21 11:33:32 +0100

    Sort out production/staging configs

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
   config.eager_load = true
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = false
   config.assets.js_compressor = :uglifier
   config.assets.compile = true
   config.log_level = :debug
@@ -12,11 +12,6 @@ Rails.application.configure do
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
-  if ENV['RAILS_LOG_TO_STDOUT'].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger = ActiveSupport::TaggedLogging.new(logger)
-  end
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: Lumumba::Application.host }
   config.action_mailer.delivery_method = :smtp
