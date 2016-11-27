30f428be46d9963d4389a19060cfbe78dc17364a
vemv <vemv@users.noreply.github.com>
2016-11-27 17:38:18 +0100

    Remove cloudinary-specific options

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,20 +1,14 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
   storage :fog unless Rails.env.in?(%w(test development))
-  process convert: 'png'
-  process tags: ['post_picture']
 
   version :standard do
-    process resize_to_fill: [100, 150, :north]
   end
 
   version :thumbnail do
-    # eager
-    # resize_to_fit(50, 50)
   end
 
   version :product do
-    process resize_to_fill: [260, 260, :north]
   end
 
 end
