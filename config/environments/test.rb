08a97beca3f233c5922c99561488b8ccfb79b408
vemv <vemv@users.noreply.github.com>
2016-12-08 16:17:00 +0100

    Test env: use settings more similar to produciton

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,12 +1,9 @@
 Rails.application.configure do
   config.cache_classes = true
-  config.eager_load = false
+  config.eager_load = true
   config.public_file_server.enabled = true
-  config.public_file_server.headers = {
-    'Cache-Control' => 'public, max-age=3600'
-  }
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
   config.action_dispatch.show_exceptions = false
   config.action_controller.allow_forgery_protection = false
   config.action_mailer.perform_caching = false
